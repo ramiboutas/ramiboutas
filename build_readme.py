301004c7081435ae44b4016200c2217419193961
Rami Boutassghount <ramiboutas@hotmail.com>
2022-10-03 22:24:54 +0200

    Update build_readme.py

--- build_readme.py
+++ build_readme.py
@@ -8,7 +8,11 @@ from bs4 import BeautifulSoup
 
 def main():
     chunks = []
+    chunks.append("## Bio\n")
     chunks.extend(get_bio())
+    chunks.append('')
+    chunks.append("## My projects\n")
+    chunks.extend(get_projects())
     chunks.append('')
     chunks.extend(get_latest_posts())
 
@@ -28,6 +32,16 @@ def get_bio():
             break
         if elem.name != "img" and str(elem).strip():
             yield str(elem).replace("welcome to my site", "welcome to my GitHub bio")
+
+
+def get_projects():
+    # Get projects from my website
+    response = requests.get("https://www.ramiboutas.com/pages/projects.html")
+    if response.status_code != 200:
+        raise ValueError("Unexpected response status code {response.status_code}")
+    
+    soup = BeautifulSoup(response.content.decode(), "html.parser")
+    heading = soup.body.find("section", id="content").find("h2")
 
 
 def get_latest_posts():
